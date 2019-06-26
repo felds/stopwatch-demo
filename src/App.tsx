6f01a91ec379d1a7e0c621a3d487db93adc7f3ab
Felds Liscia <dev@felds.com.br>
2019-06-26 16:19:48 -0300

    shake shake shake

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
 import React from "react";
 import StopWatch, { StopWatchChildren } from "@felds/stopwatch";
-import "./App.css";
+import classnames from "classnames";
 
 const RenderFunction: StopWatchChildren = ({
   value,
   toggle,
   stop,
   isRunning,
+  isFinished,
 }) => {
   const sec = Math.floor(value / 1000);
   const s = sec % 60;
@@ -14,13 +15,15 @@ const RenderFunction: StopWatchChildren = ({
   const ms = Math.floor(value % 1000);
 
   return (
-    <div className="cd">
+    <div className={classnames("cd", isFinished && "cd--is-finished")}>
       <div className="cd__display">
         <div className="cd__time">
           <span className="cd__m">{`00${m}`.substr(-2)}</span>:
           <span className="cd__s">{`00${s}`.substr(-2)}</span>
           <span className="cd__ms">.{`000${ms}`.substr(-3)}</span>
         </div>
+
+        <div className="cd__duration">01:30</div>
       </div>
 
       <div className="cd__actions">
@@ -36,9 +39,11 @@ const RenderFunction: StopWatchChildren = ({
 };
 
 function App() {
+  const duration = 90000; // 90 secs
+
   return (
     <div>
-      <StopWatch children={RenderFunction} />
+      <StopWatch children={RenderFunction} duration={duration} />
     </div>
   );
 }
